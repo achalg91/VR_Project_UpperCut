e4462d4fe76ba68c559217a91168e35144ad4f72
Sunny Ladkani <54920880+sl2883@users.noreply.github.com>
2019-11-05 22:30:15 -0500

    try1

---
+++ README.md
@@ -0,0 +1,17 @@
+### INFO 5340 / CS 5650
+### Virtual and Augmented Reality 
+# Group project - Fall 2019
+
+<hr>
+
+### Group Name and Project Title:
+- G03
+- Uppercut
+
+### Group Members:
+
+- Melanie Kaplan-Cohen, mk2573@cornell.edu, @melaniekc
+- [Insert your full name, Cornell email, GitHub handle]
+- [Insert your full name, Cornell email, GitHub handle]
+
+
---
+++ UpperCut/Assets/Scripts/SceneManagerScript.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class SceneManagerScript : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject timerText;
+
+    [SerializeField]
+    public GameObject videoScreen;
+
+
+    private TextMeshProUGUI m_textMesh;
+    private UnityEngine.Video.VideoPlayer m_videoPlayer;
+
+    private bool m_timerRunning;
+    private int m_timerCount;
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_textMesh = timerText.GetComponent<TextMeshProUGUI>();
+        m_timerRunning = true;
+        m_timerCount = 10;
+
+        setVideo();
+
+    }
+
+    void setVideo()
+    {
+        m_videoPlayer = videoScreen.GetComponent<UnityEngine.Video.VideoPlayer>();
+        videoScreen.SetActive(false);
+
+    }
+
+    void EndReached(UnityEngine.Video.VideoPlayer vp)
+    {
+        vp.playbackSpeed = vp.playbackSpeed / 10.0F;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (m_timerRunning)
+            StartCoroutine(runTimer());
+    }
+
+    private IEnumerator runTimer()
+    {
+        m_timerRunning = false;
+        while (m_timerCount > -1)
+        {
+            if (m_timerCount > 0)
+                m_textMesh.text = "" + m_timerCount--;
+            else
+            {
+                timerText.SetActive(false);
+                videoScreen.SetActive(true);
+                m_videoPlayer.Play();
+            }
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
