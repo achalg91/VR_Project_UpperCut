bef20b6b3c91c48a966499eb031316e265be6d26
melaniekc <mkaplancohen@gmail.com>
2019-11-25 19:50:24 -0500

    Punch game interactions

---
+++ Sample_VR_1/Assets/Scripts/PunchController.cs
@@ -0,0 +1,118 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PunchController : MonoBehaviour
+{
+
+    [SerializeField]
+    public GameObject jabPrefab;
+    [SerializeField]
+    public GameObject hookPrefab;
+    [SerializeField]
+    public GameObject uppercutPrefab;
+
+    private int rounds;
+    private int completed;
+    private GameObject punchTarget;
+    private bool leftActive;
+    private bool rightActive;
+
+    Transform refPoint;
+
+    private readonly int DEFAULT_ROUNDS = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rounds = 0;
+        completed = 0;
+        refPoint = Camera.main.transform;
+        Begin();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (punchTarget == null)
+        {
+            return;
+        }
+        if (leftActive)
+        {
+            //Debug.Log("UPPERCUT: USER NEEDS TO HIT LEFT");
+            if (punchTarget.GetComponent<PunchSequence>().IsDone())
+            {
+                Debug.Log("UPPERCUT: LEFT HIT!");
+                leftActive = false;
+                Destroy(punchTarget);
+                InitRight();
+            }
+        } else if (rightActive)
+        {
+            //Debug.Log("UPPERCUT: USER NEEDS TO HIT RIGHT");
+            if (punchTarget.GetComponent<PunchSequence>().IsDone())
+            {
+                Debug.Log("UPPERCUT: RIGHT HIT!");
+                rightActive = false;
+                Destroy(punchTarget);
+                CompleteRound();
+            }
+        }
+    }
+
+    private void Begin()
+    {
+        Debug.Log("UPPERCUT: GAME BEGIN");
+        if (rounds <= 0)
+        {
+            rounds = DEFAULT_ROUNDS;
+        }
+        InitLeft();
+    }
+
+    private void InitLeft()
+    {
+        Debug.Log("UPPERCUT: CREATE LEFT OBJ");
+        punchTarget = Instantiate(hookPrefab);
+        punchTarget.transform.position = refPoint.position + refPoint.up - refPoint.right;// + (Camera.main.transform.forward * 0.1f) + (Camera.main.transform.up* 5.0f);
+        punchTarget.transform.localScale *= 10;
+        punchTarget.GetComponent<PunchSequence>().Begin(true);
+        leftActive = true;
+    }
+
+    private void InitRight()
+    {
+        Debug.Log("UPPERCUT: CREATE RIGHT OBJ");
+        punchTarget = Instantiate(hookPrefab.GetComponent<PunchSequence>().Right());
+        punchTarget.transform.position = refPoint.position + refPoint.up + refPoint.right;// + (Camera.main.transform.forward * 0.12f) + (Camera.main.transform.up * 5.0f); // + (Camera.main.transform.right * 2.0f) 
+        punchTarget.transform.localScale *= 10;
+        punchTarget.GetComponent<PunchSequence>().Begin(false);
+        rightActive = true;
+    }
+
+    private void CompleteRound()
+    {
+        completed++;
+        Debug.Log("UPPERCUT: ROUND " + completed + " COMPLETE");
+        if (rounds > completed)
+        {
+            InitLeft();
+        }
+        else
+        {
+            SceneManager.LoadScene("Menu");
+        }
+    }
+
+    public void SetRounds(int x)
+    {
+        if (x >= 0)
+        {
+            this.rounds = x;
+        }
+    }
+
+
+}
---
+++ Sample_VR_1/Assets/Scripts/PunchSequence.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PunchSequence : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject front;
+    [SerializeField]
+    GameObject back;
+
+    private bool firstHit;
+    private bool secondHit;
+    private bool started;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (started)
+        {
+            if (!firstHit)
+            {
+                //Debug.Log("UPPERCUT: WAITING FOR FRONT OBJECT TO BE HIT");
+                firstHit = front.GetComponent<RegisterPunch>().WasPunched();
+            } else {
+                //Debug.Log("UPPERCUT: WAITING FOR RIGHT OBJECT TO BE HIT");
+                secondHit = back.GetComponent<RegisterPunch>().WasPunched();
+            }
+        }
+    }
+
+    public GameObject Right()
+    {
+        Debug.Log("UPPERCUT: Right!");
+        // invert the hit circles for right hand
+        float diff = Mathf.Abs(front.transform.position.x - back.transform.position.x);
+        front.transform.position = front.transform.position + front.transform.right * 2 * diff;
+        return this.gameObject;
+    }
+
+    public bool IsDone()
+    {
+        return started && firstHit && secondHit;
+    }
+
+    public void Begin(bool left)
+    {
+        Debug.Log("UPPERCUT: BEGIN PUNCH SEQUENCE");
+        front.GetComponent<RegisterPunch>().SetLeft(left);
+        back.GetComponent<RegisterPunch>().SetLeft(left);
+        this.started = true;
+    }
+}
